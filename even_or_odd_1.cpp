9997fe2878accee9f060a610161d10e2675fcc57
Bivshtex007 <103932684+Bivshtex007@users.noreply.github.com>
2022-07-18 22:48:10 +0300

    Add files via upload

---
+++ even_or_odd_1.cpp
@@ -0,0 +1,18 @@
+//even or odd
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+int number;
+cout << "Enter your Integer number" << endl;
+cin >> number;
+
+if (number % 2 == 0) {
+cout << "Your number is Odd" << endl; }
+else {
+cout << "your number is Even" << endl; }
+return 0;
+
+}
